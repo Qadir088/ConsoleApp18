8e1163e0cb47508c7a0ce98d990d5d46224ed63a
Qadir088 <qadircabbarli55@gmail.com>
2024-04-22 13:42:11 +0400

    solved

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Electronic electronic = new Electronic();
+        }
+        interface Product
+        {
+            double GetPrice(double b);
+            int ReduceStock(int i);
+
+        }
+        class Book : Product
+        {
+            public double GetPrice()
+            {
+                return 0;
+            }
+
+            public double GetPrice(double b)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int ReduceStock(int i)
+            {
+                return 0;
+            }
+        }
+        class Electronic :Product
+            {
+            public double GetPrice(double b)
+            {
+                return 0;
+            }
+
+            public int ReduceStock(int i)
+            {
+                return 0;
+            }
+        }
+        public int GetPrice()
+            {
+                return 0;
+            }
+            public void ReduceStock()
+            {
+
+            }
+    }
+}  
+
+
